b163a2b2956787a751dae2e85acffcb7efc748af
Tushar Verma <vermatushar1117@gmail.com>
2026-03-05 15:17:41 +0530

    updated porfolio pages and port config

--- website.js
+++ website.js
@@ -1,44 +1,46 @@
 const http = require("http");
 const fs = require("fs");
 
+const PORT = process.env.PORT || 9999;
+
 http.createServer((req, res) => {
 
+    let collectHeaderdata = fs.readFileSync("html/header.html", "utf-8");
 
+    let file = "home";
 
-    let collectHeaderdata=fs.readFileSync("html/header.html", "utf-8")
-
-
-    let file="/home";
-    if(req.url!=="/"){
-        file=req.url;
+    if (req.url !== "/") {
+        file = req.url.replace("/", "");
     }
 
-    if (req.url != "/style.css") {
-        fs.readFile("html"+file+".html", "utf-8", (error, data) => {
-            if (error) {
-                res.writeHead(500, { "content-type": "text/plain" })
-                res.end("internal server error")
-                return false
-            }
-console.log();
-            res.write(collectHeaderdata+""+data);
-            res.end();
-        })
-    } else if (req.url == "/style.css") {
+    // CSS handle
+    if (req.url === "/style.css") {
+
         fs.readFile("html/style.css", "utf-8", (error, data) => {
             if (error) {
-                res.writeHead(500, { "content-type": "text/plain" })
-                res.end("css not found")
-                return false
+                res.writeHead(500, { "content-type": "text/plain" });
+                res.end("CSS not found");
+                return;
             }
-            res.writeHead(200, { "content-type": "text/css" })
+            res.writeHead(200, { "content-type": "text/css" });
+            res.end(data);
+        });
 
-            res.end(data)
-        })
+    } else {
+
+        fs.readFile(`html/${file}.html`, "utf-8", (error, data) => {
+            if (error) {
+                res.writeHead(404, { "content-type": "text/plain" });
+                res.end("Page not found");
+                return;
+            }
+
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(collectHeaderdata + data);
+        });
+
     }
 
-
-
-
-
-}).listen(9999)
+}).listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
